67a5e34e0d5527289310b141c3888e7c9a7df1ed
SooryaSelvaraju <soorya.s@myactionboard.com>
2025-06-20 10:06:29 +0530

    Implement basic login

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "channels"
+import "jquery"
+import "bootstrap"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
